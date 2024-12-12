9409439dde95d89572288c6ee898f0e58de17efc
Ranindu Nethmina <ranindunethmina@gmail.com>
2024-12-12 10:20:59 +0530

    write file

--- File Handling/python.py
+++ File Handling/python.py
@@ -3,7 +3,16 @@
 # print(content, type(content))
 # file1.close()
 
-with open("File Handling\my_file.txt", "r") as file:
-    content = file.read()
-    print(content)
+# with open("File Handling\my_file.txt", "r") as file:
+#     content = file.read()
+#     print(content)
 # no need for file.close() as it's automatically handle
+
+with open("File Handling\my_file1.txt", "w") as file:
+    file.write("This is awesome.\n")
+    file.write("Programming is fun.")
+
+
+with open("File Handling\my_file2.txt", "w") as file:
+    my_list = ["This is something new.\n","This is the writelines method."]
+    file.writelines(my_list)
