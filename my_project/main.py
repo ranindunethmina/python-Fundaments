45c1c1f94164dffb0e6d7f6a5f1b588606d26158
Ranindu Nethmina <ranindunethmina@gmail.com>
2025-01-06 15:39:59 +0530

    Python packages, and the if __name__ == __main__ construct

--- my_project/main.py
+++ my_project/main.py
@@ -17,10 +17,17 @@
 # print(result1,result2)
 
 # 3rd way
-from my_calculator.addition import add
-from my_calculator.subtraction import subtract
+# from my_calculator.addition import add
+# from my_calculator.subtraction import subtract
 
-result1= add(3,4)
-result2= subtract(7,3)
+# result1= add(3,4)
+# result2= subtract(7,3)
+
+# print(result1,result2)
+
+from my_calculator import add,subtract
+
+result1 = add(3,4)
+result2 = subtract(7,3)
 
 print(result1,result2)
